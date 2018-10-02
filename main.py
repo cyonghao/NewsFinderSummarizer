5a8e674327ce863f4d1fe51caa65036a48f88627
Yong Hao <yonghao.choo@gmail.com>
2018-10-02 21:00:05 +0800

    added top headlines finder

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from tokenconfig import TOKEN
+
+def news_finder():
+    sources = 'associated-press,reuters'
+    pageSize = 10
+
+    top_headlines = newsapi.get_top_headlines(sources=sources, language='en', pageSize=pageSize)
+    return top_headlines
+
+if __name__ == '__main__':
+    newsapi = NewsApiClient(api_key=TOKEN)
